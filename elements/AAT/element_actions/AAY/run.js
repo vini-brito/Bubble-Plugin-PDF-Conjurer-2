6f8c97a9aea0352e0e046d2d1711109e2e963cf5
Vinicius Brito <6878457+vini-brito@users.noreply.github.com>
2019-12-03 11:06:23 -0400

    Automatic commit from Bubble

--- elements/AAT/element_actions/AAY/run.js
+++ elements/AAT/element_actions/AAY/run.js
@@ -1,9 +1,11 @@
 function(instance, properties, context) {
   
 // finally generates the PDF
+    
+let pasteurizedFileName = properties.file_name.replace(/^(con|prn|aux|nul|com[0-9]|lpt[0-9])$|([<>:"\/\\|?*])|(\.|\s)$/ig, ""); // sanitizing because users may pass dots or other stuff that makes the file loses its extension on Windows and other nasty side effects    
 
 instance.data.docDefinition.info = {
-    								title: `${properties.file_name}`,
+    								title: `${pasteurizedFileName}`,
     								/*author: 'john doe',
     								subject: 'subject of document',
     								keywords: 'keywords for document',*/
@@ -13,7 +15,21 @@ instance.data.docDefinition.info = {
 	
 if (instance.data.imagesAreLoadedPromise === undefined) {
 	
-	pdfMake.createPdf(instance.data.docDefinition).download(properties.file_name); // this generates the pdf file
+	pdfMake.createPdf(instance.data.docDefinition).download(pasteurizedFileName); // this generates the pdf file
+    
+    if (properties.save_to_database) { // checks if checkbox was checked
+    
+    const pdfDocGenerator = pdfMake.createPdf(instance.data.docDefinition); // and this uploads it to the bubble app storage
+    pdfDocGenerator.getBase64((data) => {
+    
+	context.uploadContent(properties.file_name, data, function(err, url) {
+        instance.publishState("saved_pdf", url) ; // these are just one argument of this callback
+        instance.triggerEvent("pdf_uploaded_and_available_in_element_state" // these are just one argument, I broke the line for readability
+                              , function(err){console.log(err)}) // this is actually the second argument of this callback
+    }, properties.attach_pdf_to)
+
+        });
+    }
 	
 } else {
 	
@@ -22,7 +38,21 @@ if (instance.data.imagesAreLoadedPromise === undefined) {
 // Thus only loading async stuff (images) after the pdf was generated
 	
 instance.data.imagesAreLoadedPromise.then(function(result) {
- 	pdfMake.createPdf(instance.data.docDefinition).download(properties.file_name); // this generates the pdf file	
+ 	pdfMake.createPdf(instance.data.docDefinition).download(pasteurizedFileName); // this generates the pdf file	
+   
+    if (properties.save_to_database) { // checks if checkbox was checked
+    
+    const pdfDocGenerator = pdfMake.createPdf(instance.data.docDefinition); // and this uploads it to the bubble app storage
+    pdfDocGenerator.getBase64((data) => {
+    
+	context.uploadContent(properties.file_name, data, function(err, url) {
+        instance.publishState("saved_pdf", url) ; // these are just one argument of this callback
+        instance.triggerEvent("pdf_uploaded_and_available_in_element_state" // these are just one argument, I broke the line for readability
+                              , function(err){console.log(err)}) // this is actually the second argument of this callback
+    }, properties.attach_pdf_to)
+
+        });
+    }
 }, function(err) {
 	if (err === undefined || err === null) {
 		err = "an image wasnt loaded, this also happens because none was asked to, in this case just ignore this";
